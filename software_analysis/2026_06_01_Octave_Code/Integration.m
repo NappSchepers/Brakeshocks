366cc68f54025c7f09886e07e500ed378f082366
NappSchepers <violarah@gmx.de>
2026-06-09 08:53:57 +0200

    Delete software_analysis/2026_06_01_Octave_Code directory

--- software_analysis/2026_06_01_Octave_Code/Integration.m
+++
@@ -1,7 +0,0 @@
-function [amplitude_integrated] = Integration(time, amplitude)
-
-amplitude_integrated = cumtrapz(time, amplitude);
-amplitude_integrated = detrend(amplitude_integrated,'constant');
-
-end
-
